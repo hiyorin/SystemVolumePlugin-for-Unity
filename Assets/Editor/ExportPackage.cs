7025c79352bc073830e7db70afe6fe575c7cde36
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2018-07-30 03:24:53 +0900

    [add] export script

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExportPackage
+{
+    private readonly static string[] Paths = {
+        "Assets/Plugins/SystemVolumePlugin",
+    };
+
+    [MenuItem("Assets/Export SystemVolumePlugin")]
+    private static void Export()
+    {
+        AssetDatabase.ExportPackage(Paths, "SystemVolumePlugin.unitypackage", ExportPackageOptions.Recurse);
+        Debug.Log("Export complete!");
+    }
+}
