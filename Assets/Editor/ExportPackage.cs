ed28a7cd7dacf594befe06d0b6ed3eacaf529192
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2018-08-05 19:56:31 +0900

    [add] include readme, license into unitypackage

--- Assets/Editor/ExportPackage.cs
+++ Assets/Editor/ExportPackage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEditor;
 
 public class ExportPackage
@@ -7,10 +8,25 @@ public class ExportPackage
         "Assets/Plugins/SystemVolumePlugin",
     };
 
+    private const string ReadMe = "README.md";
+    private const string License = "LICENSE";
+
     [MenuItem("Assets/Export SystemVolumePlugin")]
     private static void Export()
     {
-        AssetDatabase.ExportPackage(Paths, "SystemVolumePlugin.unitypackage", ExportPackageOptions.Recurse);
+        string readmePath = Path.Combine(Application.dataPath, "Plugins/SystemVolumePlugin", ReadMe);
+        string licensePath = Path.Combine(Application.dataPath, "Plugins/SystemVolumePlugin", License);
+        File.Copy(Path.Combine(Application.dataPath, "..", ReadMe), readmePath);
+        File.Copy(Path.Combine(Application.dataPath, "..", License), licensePath);
+        AssetDatabase.Refresh();
+
+        AssetDatabase.ExportPackage(Paths, "SystemVolumePlugin-for-Unity.unitypackage", ExportPackageOptions.Recurse);
         Debug.Log("Export complete!");
+
+        File.Delete(readmePath);
+        File.Delete(licensePath);
+        File.Delete(readmePath + ".meta");
+        File.Delete(licensePath + ".meta");
+        AssetDatabase.Refresh();
     }
 }
